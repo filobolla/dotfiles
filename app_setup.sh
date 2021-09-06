d15039fb2efd352f1a1fd7d00ddfc2dffbd01df3
Filippo Bollarino <13319932+filobolla@users.noreply.github.com>
2021-09-06 18:09:58 +0200

    Unhide macos setup scripts

---
+++ app_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Install vscode extensions
+cat vs_code_extensions_list.txt | xargs -n 1 code --install-extension
+
+# TODO:
+# - Add favorites apps to Dock
+# - Add favorites folders to Finder
+# - Scripting Launchpad folder creations
