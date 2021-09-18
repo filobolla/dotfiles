6e4734c090487af05a5a30fecb3aa8c95cb22df8
Filippo Bollarino <13319932+filobolla@users.noreply.github.com>
2021-09-18 18:04:24 +0200

    Updated README and app setup (bookmarks and references)

--- app_setup.sh
+++ app_setup.sh
@@ -4,7 +4,16 @@
 cat vs_code_extensions_list.txt | xargs -n 1 code --install-extension
 
 # TODO:
-# - Add favorites folders to Finder
+# - Add favorites folders and toolbar items to Finder
 # - Scripting Launchpad folder creations
+
 # - Import Cyberduck bookmarks
-# - Import Brave bookmarks
+# Check if Cyberduck is running, then move Bookmark folder
+if test find /Applications -type d -maxdepth 1 -name "Cyberduck.app">/dev/null; then
+# mv Bookmarks "${HOME}/Library/Application Support/CyberDuck/"
+fi
+
+# - Import Brave bookmarks and preferences
+if test find /Applications -type d -maxdepth 1 -name "Brave Browser.app">/dev/null; then
+# mv Bookmarks "${HOME}/Library/Application Support/BraveSoftware/Brave-Browser/Default/"
+# mv Preferences "${HOME}/Library/Application Support/BraveSoftware/Brave-Browser/Default/"
