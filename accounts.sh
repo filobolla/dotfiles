e058845b637d1033209b1afb6f956f8bc0dcc4ee
Filippo Bollarino <13319932+filobolla@users.noreply.github.com>
2021-11-28 11:40:57 +0100

    Deleted mail_prefs

--- accounts.sh
+++ accounts.sh
@@ -2,5 +2,6 @@
 
 osascript << EOD
 tell application "Mail"
-	get properties of every smtp server
+	get properties of account "Poli"
+--	set newmail to make new imap account with properties {name:"Poli", full name:"Filippo Bollarino", email addresses:"s287670@studenti.polito.it"}
 end tell
