e928c62bc80b18c0418035c898147879e1bd0753
Filippo Bollarino <13319932+filobolla@users.noreply.github.com>
2021-12-05 11:34:46 +0100

    Added Mail backup

--- accounts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-osascript << EOD
-tell application "Mail"
-	get properties of account "Poli"
---	set newmail to make new imap account with properties {name:"Poli", full name:"Filippo Bollarino", email addresses:"s287670@studenti.polito.it"}
-end tell
--- backups_folders.sh
+++ backups_folders.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Wallpapers backup for <10.11
-rsync -auhP --exclude=.* --exclude=*.madesktop "/Library/Desktop Pictures/" "/Volumes/Public/Filippo/dotfiles_backup/Wallpapers/"
+#rsync -auhP --exclude=.* --exclude=*.madesktop "/Library/Desktop Pictures/" "/Volumes/Public/Filippo/dotfiles_backup/Wallpapers/"
 
 # Backup massCode snippets
+echo -e "Backup massCode snippets... \n"
 rsync -auhP --exclude=backups/ "$HOME/massCode/" "/Volumes/Public/Filippo/dotfiles_backup/massCode/"
+
+echo -e "Backup Mail folders... \n"
+rsync -auhP --exclude="Deleted Messages.mbox" "${HOME}/Library/Mail/" "/Volumes/Public/Filippo/dotfiles_backup/Mail/"
