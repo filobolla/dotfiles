b04014456922677dc586b587e0a0be62d319ff6c
Filippo Bollarino <13319932+filobolla@users.noreply.github.com>
2021-12-22 16:37:49 +0100

    mail_backup: Added remote auto mount

--- mail_backup.sh
+++ mail_backup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# If Public folder isnot mounted, grab password and 
+if test ! -d "/Volumes/Public" ; then
+pw=$(security 2>&1 >/dev/null find-internet-password -a filippo -l "CasaBolla-NAS" -g | cut -d'"' -f2)
+
+open 'smb://filippo:'"${pw}"'@casabolla-nas/Public'
+# unset variable to avoid pw leaks
+unset pw
+
+fi
+
+sleep 2
+# If folder continues to be missing, abort
+if test ! -d "/Volumes/Public" ; then
+	echo "Public folder continues to be missing, aborting backup..."
+	exit 1
+else
 # Get Mail path via Applescript
 ids=$(osascript << EOD 
 tell application "Mail"
@@ -27,3 +43,4 @@ arrName=($(echo "$names" | sed 's/ /-/g' | sed 's/\,-/ /g'))
 for index in "${!arrName[@]}"; do
 	rsync -auhP --exclude="Deleted*.mbox" --exclude="Drafts.mbox" "${paths[index]}/" "/Volumes/Public/Filippo/dotfiles_backup/Mail/${arrName[index]}/" --delete-excluded --delete-after
 done
+fi
